c1bada8eeccd2aac167ef0436ef0fa9471a4384c
Student <student@sae.de>
2026-04-25 12:32:14 +0200

    feat: armor Methods and summarys
    Equip, Get, Unequip

--- Assets/Scripts/Backpack/Backpack.cs
+++ Assets/Scripts/Backpack/Backpack.cs
@@ -8,11 +8,14 @@ public class Backpack : MonoBehaviour
     private List<Buff> m_buffs = new List<Buff>();
 
     private Weapon m_equipedWeapon;
+    private Armor m_equipedArmor;
 
     public Weapon EquipedWeapon => m_equipedWeapon;
+    public Armor EquipedArmor => m_equipedArmor;
+    
 
     /// <summary>
-    /// returns Item in Backpack
+    /// returns equipables in Backpack
     /// </summary>
     /// <param name="_index">index of Item</param>
     /// <returns></returns>
@@ -24,21 +27,34 @@ public class Backpack : MonoBehaviour
         return m_equipables[_index];
     }
 
+    /// <summary>
+    /// Removes Equipable by item from Backpack
+    /// </summary>
+    /// <param name="_equipable">Item to remove</param>
     public void RemoveEquipable(IAmEquipable _equipable)
     {
         m_equipables.Remove(_equipable);
     }
 
+    /// <summary>
+    /// Removes Equipable by index from Backpack
+    /// </summary>
+    /// <param name="_index"></param>
     public void RemoveEquipable(int _index)
     {
+        if (_index < m_equipables.Count)
+            throw new IndexOutOfRangeException("index out of range");
         m_equipables.RemoveAt(_index);
     }
 
+    /// <summary>
+    /// Adds Equipable to Backpack
+    /// </summary>
+    /// <param name="_equipable">the item to equip</param>
     public void AddEquipable(IAmEquipable _equipable)
     {
         m_equipables.Add(_equipable);
     }
-
 
     /// <summary>
     /// returns Buff in Backpack
@@ -48,11 +64,15 @@ public class Backpack : MonoBehaviour
     /// <exception cref="IndexOutOfRangeException"></exception>
     public Buff GetBuff(int _index)
     {
-        if (_index > m_equipables.Count)
+        if (_index < m_equipables.Count)
             throw new IndexOutOfRangeException("_index out of range");
         return m_buffs[_index];
     }
 
+    /// <summary>
+    /// Returns all Weapons in List
+    /// </summary>
+    /// <returns></returns>
     public IAmEquipable[] GetWeapons()
     {
         var weapons = m_equipables.FindAll((IAmEquipable q) => q is Weapon);
@@ -60,14 +80,67 @@ public class Backpack : MonoBehaviour
         return weapons.ToArray();
     }
 
+    /// <summary>
+    /// Equip weapon
+    /// </summary>
+    /// <param name="_weapon">The Weapon to Equip</param>
     public void EquipWeapon(Weapon _weapon)
     {
-        m_equipedWeapon = _weapon;
+        bool weaponIsInList = false;
+        foreach (IAmEquipable item in m_equipables)
+        {
+            if (item == _weapon)
+            {
+                weaponIsInList = true;
+                break;
+            }
+        }
+        if (weaponIsInList)
+            m_equipedWeapon = _weapon;
+        else
+            throw new InvalidOperationException(_weapon + "is not in List");
     }
 
-    public void UnEquipWeapon()
+    /// <summary>
+    /// sets equipedWeapon null
+    /// </summary>
+    public void UnequipWeapn()
     {
         m_equipedWeapon = null;
+    }
+
+    /// <summary>
+    /// Returns all Armors in List
+    /// </summary>
+    /// <returns>IAmEquipable[]</returns>
+    public IAmEquipable[] GetArmor()
+    {
+        var armors = m_equipables.FindAll((IAmEquipable q) => q is Armor);
+
+        return armors.ToArray();
+    }
+
+
+    public void EquipArmor(Armor _armor)
+    {
+        bool ArmorIsInList = false;
+        foreach (IAmEquipable item in m_equipables)
+        {
+            if (item == _armor)
+            {
+                ArmorIsInList = true;
+                break;
+            }
+        }
+        if (ArmorIsInList)
+            m_equipedArmor = _armor;
+        else
+            throw new InvalidOperationException(_armor + "is not in List");
+    }
+
+    public void UnequipArmor()
+    {
+        m_equipedArmor = null;
     }
 
 }
