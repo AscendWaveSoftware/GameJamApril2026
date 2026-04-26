3dd9f48e341ec09b1caed51d3f2dd0331a861a86
SAE Studi <49801250+alystor@users.noreply.github.com>
2026-04-26 14:49:47 +0200

    WIP

--- Assets/Scripts/GameLoop/OnWaitingToStartNightTimePhaseHandler.cs
+++ Assets/Scripts/GameLoop/OnWaitingToStartNightTimePhaseHandler.cs
@@ -1,3 +1,4 @@
+using Currency;
 using Player;
 using UnityEngine;
 
@@ -10,7 +11,20 @@ namespace GameLoop
         public void Handle(GameLoopManager manager)
         {
             manager.ApplyRealSecondsPerGameMinute(0f);
+            RemoveAllSpawnedCoins();
             ScreenTitleManager.ShowTitleUntilKey("Night is ready. Press E to start.", KeyCode.E);
+        }
+
+        private static void RemoveAllSpawnedCoins()
+        {
+            GoldCoin[] coins = Object.FindObjectsByType<GoldCoin>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            for (int i = 0; i < coins.Length; i++)
+            {
+                if (coins[i] != null)
+                {
+                    Object.Destroy(coins[i].gameObject);
+                }
+            }
         }
     }
 }
